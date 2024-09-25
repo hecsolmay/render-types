c128ae0fad2fa503dff0165b854d6ed232c9c4c0
hecsolmay <hecsolmay@gmail.com>
2024-09-24 20:15:48 -0600

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home () {
   return (
-    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
-
-    </div>
+    <main className='mx-auto min-h-screen text-left'>
+      <div className='px-8 py-64 sm:px-28 md:px-64 md:py-52'>
+        <h1 className='pb-4 text-4xl font-bold text-white md:text-6xl'>
+          Pagina de inicio
+        </h1>
+        <p className='text-xl font-semibold text-white md:pb-1 md:text-3xl'>
+          Esta parte fue generada en el servidor.
+        </p>
+        <p className='text-lg  text-neutral-300  md:text-xl'>
+          En esta web se muestra como se renderizan las paginas tanto de parte
+          del servidor como en el cliente
+        </p>
+      </div>
+    </main>
   )
 }
